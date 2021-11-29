5c62716b98a36f5d896cd76f9df552dd090d4bb3
Minh Nguyen <thn649@usask.ca>
2021-11-29 16:03:35 -0600

    Completed backend stuffs.

--- server/server.js
+++ server/server.js
@@ -10,8 +10,8 @@ const mongoose = require("mongoose");
 //import the models
 const Staff = require('./model/Staff');
 const Customer = require('./model/Customer');
-const staff_position = require('./model/staff-position');
-const Room = require('./model/staff-position');
+const Position = require('./model/staff-position');
+const Room = require('./model/rooms');
 
 const port = 8080;
 
@@ -34,19 +34,16 @@ mongoose.connect(shelterURL, {useNewUrlParser: true, useUnifiedTopology: true})
 })
 .catch(error => throwError(error));
 
-
-//---------------- Staff methods ----------------//
+//---------------- Staff ----------------//
 
 // creating a staff member
 app.post('/addStaff', (req, res) => {
-    let name = req.body.name;
-    let position = req.body.position;
-    let id = Math.floor(Math.random() * 100) + 1; // for now I will generate a random number for ID
 
     const newStaff = new Staff({
-        name: name,
-        position: position,
-        id: id
+        name: req.body.name,
+        position: req.body.position,
+ // for now I will generate a random number for ID
+        id: Math.floor(Math.random() * 998) + 1 
     });
 
     newStaff.save()
@@ -58,10 +55,56 @@ app.post('/addStaff', (req, res) => {
     });
 })
 
+// a post method to update a staff's new position
+app.post('/updateStaff', (req, res) => {
+    const filter = {id: req.body.staffID}; // filter to find the document with matching id
+    const updatePosition = {$set: { position: req.body.newPosition }}; // replace with the new position
+    Staff.updateOne(filter, updatePosition, err => {
+        if (err) throwError(err);
+        res.send(JSON.stringify("Update Complete!"));
+    })
+});
+
+// a post method to delete a staff from staff table when staff's id is given
+app.post('/deleteStaff', (req, res) => {
+    const filter = { id: req.body.deleteStaffID };
+    Staff.deleteOne(filter, err => {
+        if (err) throwError(err);
+        res.send(JSON.stringify("Delete Success!"));
+    });
+});
+
+// a post method to retrieve a staff members info
+app.post('/staffInfo', (req, res) => {
+    const filter = { id: req.body.id };
+    Staff.findOne(filter)
+    .then(result => {
+        if (result != null) {
+            let info = {
+                name: result.name,
+                id: result.id,
+                position: result.position
+            }
+            res.send(JSON.stringify(info));
+        }
+    })
+    .catch(err => throwError(err));
+});
+
+// a get method to send a list of employees and their IDs
 app.get('/employeeList', (req, res) => {
     Staff.find()
     .then(result => {
-        console.log(result);
+        let nameIDs = [];
+        Object.keys(result).forEach(key => {
+            let row = result[key];
+            let nameID = {
+                name: row.name,
+                id: row.id
+            }
+            nameIDs.push(nameID);
+        });
+        res.send(JSON.stringify(nameIDs));
     })
     .catch(err => {
         throwError(err);
@@ -69,9 +112,9 @@ app.get('/employeeList', (req, res) => {
 });
 
 
-
+// a get method to send a list of staff positions
 app.get('/positionList', (req, res) => {
-    staff_position.find()
+    Position.find()
     .then(result => {
         res.send(JSON.stringify(result));
     })
@@ -81,8 +124,186 @@ app.get('/positionList', (req, res) => {
 })
 
 
+//------------------------ Customers ------------------------//
+// a post method to add a new customer 
+app.post('/addCustomer', (req, res) => {
+    const name = req.body.name;
+    const room_num = req.body.room_num;
+    const id = Math.floor(Math.random() * 998) + 1;
+    const check_in = Date.now();
+    const log = req.body.log;
+
+    const newRoom = new Customer({
+        name: name,
+        id: id,
+        room_num: room_num,
+        check_in: check_in,
+        check_out: null,
+        log: log,
+    });
+
+    // add the new customer to customers collection
+    newRoom.save()
+    .catch(err => {
+        throwError(err)
+    });
+
+    // add the customer's id to rooms collection
+    const filter = { room_num: room_num }
+    const updateRoom = { $set: { id: id }};
+
+    Room.updateOne(filter, updateRoom, err => {
+        if (err) throwError(err);
+    })
+    res.send(JSON.stringify("Request complete!"));
+});
 
 
+// a post method to update customers to a new room
+app.post('/updateCustomers', (req, res) => {
+    let customerID = req.body.customerID;
+    let newRoom = req.body.newRoom;
+    let newLog = req.body.log;
+
+    if (newRoom != null) {
+        // update the current room to 0 first
+        let filter = { id: customerID };
+        const deleteRoom = { $set: { id: 0 } };
+        Room.updateOne(filter, deleteRoom, err => {
+            if (err) throwError(err);
+        });
+
+        // add the customer ID to the new room
+        filter = { room_num: newRoom };
+        const updateID = { $set: { id: customerID } };
+        Room.updateOne(filter, updateID, err => {
+            if (err) throwError(err);
+        });
+
+        // then update the new room_num in the customers collection
+        filter = { id: customerID };
+        const updatenewRoom = { $set: { room_num: newRoom } };
+        Customer.updateOne(filter, updatenewRoom, err => {
+            if (err) throwError(err);
+        });
+        res.send(JSON.stringify("successfully assign the customer to the new "))
+    }
+
+    // add a new log to the customer
+    if (newLog != null) {
+        const filter  = { id: customerID };
+        Customer.findOne(filter)
+        .then(result => {
+            let log = result.log + `\nnew Entry: ${newLog}`;
+            const addLog = { $set : {log: log } };
+
+            Customer.updateOne(filter, addLog, err => {
+                if (err) throwError(err);
+                res.send(JSON.stringify("Request Complete"))
+            })
+        })
+    }
+});
+
+// a post method to delete a given customer
+app.post('/deleteCustomers', (req, res) => {
+    let deleteID = req.body.deleteCustomerID;
+    const filter = { id: deleteID };
+    const deleteRoom = { $set: { id: 0 } };
+    Room.updateOne(filter, deleteRoom, err => {
+        if (err) throwError(err);
+    })
+
+    Customer.deleteOne(filter, err => {
+        if (err) throwError(err);
+    })
+    res.send(JSON.stringify("Request Complete"));
+});
+
+// a post method to checkout a customer
+app.post('/checkoutCustomer', (req, res) => {
+    let id = req.body.id;
+    let filter = { id: id };
+    const check_out = { $set: { check_out: Date.now() } };
+    let roomNum;
+    Customer.findOne(filter)
+    .then(result => {
+        roomNum = result.room_num; // get the room number to update in rooms collection
+    })
+    .catch(err => throwError(err));
+
+    Customer.updateOne(filter, check_out, err => {
+        if (err) throwError(err);
+    });
+
+    filter = { room_num: roomNum };
+    const deleteRoomID = { $set: { id: 0 } };
+    
+    Room.updateOne(filter, deleteRoomID, err => {
+        if (err) throwError(err);
+    });
+    
+    res.send(JSON.stringify("Checkout complete"));
+});
+
+// a post method to retrieve a customer's info
+app.post('/customerInfo', (req, res) => {
+    let id = req.body.id;
+    const filter = { id: id };
+    Customer.findOne(filter)
+    .then(result => {
+        if (result != null) {
+            let info = {
+                name: result.name,
+                id: result.id,
+                room_num: result.room_num,
+                check_in: result.check_in,
+                check_out: result.check_out,
+                log: result.log
+            }
+            res.send(JSON.stringify(info));
+        }
+    })
+    .catch(err => {
+        throwError(err);
+    });
+});
+
+
+
+// a get method to send a list of customers and their IDs
+app.get('/customerList', (req, res) => {
+    Customer.find()
+    .then(result => {
+        let nameIDs = [];
+        Object.keys(result).forEach(key => {
+            let row = result[key];
+            let nameID = {
+                name: row.name,
+                id: row.id
+            }
+            nameIDs.push(nameID);
+        });
+        res.send(JSON.stringify(nameIDs));
+    })
+    .catch(err => {
+        throwError(err);
+    });
+});
+
+
+
+app.get('/roomList', (req, res) => {
+    Room.find().sort( { room_num: 1}) // 1 is ascending order
+    .then(result => {
+        let rooms = [];
+        Object.keys(result).forEach(key => {
+            let room = result[key].room_num;
+            rooms.push(room);
+        })
+        res.send(JSON.stringify(rooms));
+    })
+});
 
 
 
