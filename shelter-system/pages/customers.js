2ba61cabc70aebc5a3e4c8d0413dd54c8d20679e
jordynesp <93738138+jordynesp@users.noreply.github.com>
2021-12-02 12:59:46 -0600

    Reformatted test get request into getStaticProps().

--- shelter-system/pages/customers.js
+++ shelter-system/pages/customers.js
@@ -1,15 +1,23 @@
 import Nav from '../components/Nav';
-import App from '../components/App';
 import styles from '../styles/Home.module.css';
 
-
-export default function CustomerHome() {
+export default function CustomerHome({ customers }) {
 
     return (
         <div className={styles.container}>
             <Nav />
-            <App />
+            <h1>List of Customers</h1>
+            <ul>
+                {customers.map(customer => 
+                    <h2>{`${customer.name}: ${customer.id}`}</h2>
+                )}
+            </ul>
         </div>
     )
 }
 
+export async function getStaticProps(context) {
+    const response = await fetch('http://server:8080/api/customerList');
+    const customers = await response.json();
+    return { props: { customers } }
+}
--- shelter-system/services/customerList.js
+++
@@ -1,6 +0,0 @@
-export async function customerList() {
-
-    const response = await fetch('http://localhost:8080/api/customerList');
-    return await response.json();
-
-}
